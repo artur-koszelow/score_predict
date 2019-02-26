c77bd3eb11a668eca777490f9bf755f4029ec0d9
Artur Koszelow <artur.koszelow@gmail.com>
2019-02-26 09:33:29 +0100

    added new function wait4tag

--- modules.py
+++ modules.py
@@ -12,6 +12,14 @@ def wait4tags(driver, tags_name: str):
         pass
 
 
+def wait4tag(driver, tag_name: str):
+    try:
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.TAG_NAME, tag_name)))
+    except TimeoutException:
+        print('tags {} load too long'.format(tag_name))
+        pass
+
+
 def wait4xpath(driver, xpath: str):
     try:
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, xpath)))
